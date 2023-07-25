507c89a17b20a54d7c9d4fb6c7e287b84303444b
Ibukunoluwa <109559225+AdemiluaIbukunoluwa@users.noreply.github.com>
2023-07-25 13:31:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
+   useEffect(()=>{
+    fetch("https://api.weatherapi.com/v1/forecast.json?key=a70d45887f63445ab57210911232407&q=Lagos&aqi=yes")
+    .then(response => response.json())
+    .then((data) => console.log(data))
+    }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Show</button>
     </div>
   );
 }
 
-export default App;
+
+export default App
