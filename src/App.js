94806aecaf4bb120fe9ce6d16949e0e8e0d35311
Ibukunoluwa <109559225+AdemiluaIbukunoluwa@users.noreply.github.com>
2023-07-25 17:26:03 +0100

    add side bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import SideBar from "./components/sidebar/sidebar.component";
+import "./App.css"
 
 function App() {
-   useEffect(()=>{
-    fetch("https://api.weatherapi.com/v1/forecast.json?key=a70d45887f63445ab57210911232407&q=Lagos&aqi=yes")
+  const [locData, setLocData] = useState([])
+  const [weatherData, setWeatherData] = useState([])
+
+  useEffect(()=>{
+    fetch("https://api.weatherapi.com/v1/current.json?key=a70d45887f63445ab57210911232407&q=Lagos&aqi=yes")
     .then(response => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      setLocData(data.location)
+      setWeatherData(data.current)
+    })
     }, [])
+
+
   return (
     <div className="App">
-      <button>Show</button>
+      {
+          console.log(locData)
+      }
+      <SideBar locationData={locData}/>
     </div>
   );
 }
---
+++ src/components/date/datedata.component.jsx
@@ -0,0 +1,13 @@
+var date = new Date()
+
+const FullDate = () => {
+    const daysOfWeek = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const day = daysOfWeek[date.getDay()]
+    const month = Months[date.getMonth()]
+    const datenum = date.getDate()
+    const fullDatestr = `${day}, ${month} ${datenum}, ${date.getFullYear()}`
+    return <p>{fullDatestr}</p>
+}
+
+export default FullDate
